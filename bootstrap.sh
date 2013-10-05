e7093cf29413654611312d1f67aa60c695fde749
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-10-04 17:14:37 -0700

    bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+RELEASE=$1
+if [ -z "$RELEASE" ]; then
+    RELEASE=master
+fi
+wget https://github.com/miguelgrinberg/nube/archive/$RELEASE.tar.gz
+tar xvzf $RELEASE.tar.gz
+rm $RELEASE.tar.gz
+cd nube-$RELEASE
+./nube install python files
